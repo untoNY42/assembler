beafc12adfcd20d7b116fdbbfd595579ba1aea81
untoNY42 <username.toNY42@gmail.com>
2026-03-13 10:10:40 +0300

    Update FASMHello.asm

--- FASMHello.asm
+++ FASMHello.asm
@@ -2,8 +2,10 @@ extern io_get_dec, io_print_dec
 
 section .bss
     array resd 1000000
-    tmp resd 3
     n resd 1
+    
+section .data
+    tmp dd -2147483648, -2147483648, -2147483648
 
 
     
@@ -15,82 +17,19 @@ main:
     
     call io_get_dec
     mov dword[n], eax
-    xor esi, esi
-    xor ebx, ebx; текущийиндес в tmp
+    xor esi, esi; i = 0
     
-    ;esi = i = 0
 .loop:
     cmp esi, dword[n]
-    jge .loop_end; если i >= n выходим
+    jge .loop_end; если i >= n
+    
     call io_get_dec
-    mov dword[array + esi*4], eax
     
-    
-    cmp esi, 3; заполняем tmp первыми тремя элементами
-    jl .tmp_append
-  
-
-    .tmp_append:
-        mov dword[tmp + esi*4], eax
-        inc esi; ++i
-        jmp .loop
-    
-    mov eax, dword[tmp + ebx*4]; eax = текущий минимальный
-    cmp dword[array + esi*4], eax
-    jg .replace
-    .replace:
-        ;обновили tmp и вошли в цикл
-        mov eax, dword[array + esi*4]
-        mov dword[tmp + ebx*4], eax
-        inc esi; ++i
-        jmp .loop
-        
-    
-    
-    inc esi; ++i
-    jmp .loop
-   
-   
-   
-.find_min:
-    mov eax, dword[tmp]
-    cmp eax, dword[tmp + 4]; a | b
-    ;eax = a
-    jle .ac
-    
-    ;a > b
-    mov eax, dword[tmp + 4]
-    cmp eax, dword[tmp + 8]; b | c
-    jle .init_b
-    
-    mov ebx, 2
-    jmp .end
-    .init_b:
-        mov ebx, 1
-        jmp .end
-        
-        
-    .ac:
-        cmp eax, dword[tmp + 8]
-        jle .init_a
-        .init_a:
-            xor ebx, ebx
-            jmp .end
-    
-
-    
-    
-    
-            
-        
-    
-.loop_end:  ;значения записаны в массив
     jmp .find_min
     
+.loop_end:   
     
-    .end:
-        mov eax, dword[tmp + ebx*4]
-        call io_print_dec
+    
     
     
     xor eax, eax
