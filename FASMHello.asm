cd5e15cbfd16b1477bbc0c58b66ed60a561f9cdc
untoNY42 <username.toNY42@gmail.com>
2026-03-24 00:30:24 +0300

    Update FASMHello.asm
    поменять факториал

--- FASMHello.asm
+++ FASMHello.asm
@@ -1,113 +1,83 @@
-;Ограничение времени:	1 с
-;Ограничение реального времени:	5 с
-;Ограничение памяти:	64M
-;Оставшиеся посылки:	24
-;Задача 03-9: Прямоугольник
-;В данной задаче вам необходимо проверить, лежит ли точка P внутри прямоугольника ABCD.
-
-;На вход программе подаются 5 пар чисел: сначала координаты вершин прямоугольника, 
-;а затем координаты точки P. 
-;Все координаты целые и по модулю не превосходят 1000. 
-;Вершины прямоугольника могут быть перечислены в произвольном порядке. 
-;Гарантируется, что стороны прямоугольника параллельны осям координат.
-
-;Программа должна вывести YES, если точка P лежит строго 
-;внутри прямоугольника ABCD, и NO в противном случае.
-
-
-
-
-extern io_get_dec, io_print_string
+extern io_get_dec, io_print_dec
 
 section .bss
-    ;весде 1я координата - ось Ох, 2я - ось Оу
-    array resd 10; array = [ax, ay, bx, by, ..., px, py]
-    
+    n resd 1
+    k resd 1
+    m resd 1
+    ;m_1 resd 1
+
 section .data
-    str_y: db 'YES', 0
-    str_n: db 'NO', 0
-    
+    res dd 0
 
 section .text
 global main
 main:
     mov ebp, esp; for correct debugging
     ;write your code here
+    ;
+    call io_get_dec
+    mov dword[n], eax
+    call io_get_dec
+    mov dword[k], eax
+    ;
+    xor ecx, ecx
+    ;ищем позицию старшего бита в числе n (m)
+.loop1:
+    shr dword[n], 1
+    cmp dword[n], 0
+    je .find_m
+    inc ecx
+    jmp .loop1
     
-    ;поиск минимума по x
-    mov eax, dword[array]; x1 - минимум
-    cmp eax, dword[array + 4*2]; x1 > x2?
-    jg .update_x1; обновляем минимум 
-    cmp eax, dword[array + 4*2]; x1 == x2?
-    je .eq1
-    mov edx, dword[array + 4*2]; x2 - максимум по х
-.eq1:
-    cmp eax, dword[array + 4*4]; x 1 > x3?
-    jg .update_x2; обновляем минимум
-    ; x1 < x3
-    mov esi, eax; esi = x1
-    mov edx, dword[array + 4*6]; edx = x4 
-    jmp .end_find_x
     
-.updata_x1:
-    mov eax, dword[array + 4*2]; eax = x2
-    mov edx, dword[array]; edx = x1 - максимум по x
-    jmp .end_find_x
+.find_m:
+    ;считаем сочетания
+    ;for(i = k; i < M; ++i){res += С из i по k}
+    mov dword[m], ecx
+    mov ecx, dword[k]
+    ;mov eax, dword[m]
+    ;dec eax; eax = m - 1
+    ;mov dword[m_1], eax
+    
+.loop2:
+    cmp ecx, dword[m]
+    ja .q2; i > m - 1
+    
+    mov eax, ecx;eax = i
+    mov ebx, eax
+    sub ebx, dword[k]
+    inc ebx; ebx = n - k + 1
+.i_fac:
+    cmp eax, ebx
+    jle .end_i_fac
+    mul eax - 1
+    dec eax
+    jmp .i_fac
+.end_i_fac:
+    mov esi, eax; esi = n*...*(n-k+1)
+    ;k!
+    mov eax, dword[k]
+.k_fac:
+    cmp eax, 1
+    jle .end_k_fac
 
-.update_x2:
-    mov eax, dword[array + 4*4]; eax = x3
-    jmp .end_find_x
+    mul eax - 1
+    dec eax
+    jmp .k_fac
+    
+            
+.end_k_fac:
+    mov edi, eax; edi = k!
+    mov eax, esi
+    xor edx, edx
+    div edi
+    add dword[res], eax
+    
 
-.end_find_x:
-    
-    
-    
-    ;;;;;;;;;;;;;;;;;
-    ;поиск минимума по y
-    mov eax, dword[array]; x1 - минимум
-    cmp eax, dword[array + 4*2]; x1 > x2?
-    jg .update_x1; обновляем минимум 
-    cmp eax, dword[array + 4*2]; x1 == x2?
-    je .eq1
-    mov edx, dword[array + 4*2]; x2 - максимум по х
-.eq1:
-    cmp eax, dword[array + 4*4]; x 1 > x3?
-    jg .update_x2; обновляем минимум
-    ; x1 < x3
-    mov esi, eax; esi = x1
-    mov edx, dword[array + 4*6]; edx = x4 
-    jmp .end_find_x
-    
-.updata_x1:
-    mov eax, dword[array + 4*2]; eax = x2
-    mov edx, dword[array]; edx = x1 - максимум по x
-    jmp .end_find_x
+.q2:
 
-.update_x2:
-    mov eax, dword[array + 4*4]; eax = x3
-    jmp .end_find_x
-
-.end_find_x:
+    
     
     
     xor eax, eax
     ret
-    
-;1 2
-;1 0
-;3 2
-;3 0
-;2 3 - >NO
-
-;1 2
-;1 0
-;3 2
-;3 0
-;2 1 - >YES
-
-;1 2
-;1 0
-;3 2
-;3 0
-;2 0 -> NO
-    
