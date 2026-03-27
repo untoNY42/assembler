bdd09d775ae0aa265d617612378c57d2ed7030c3
untoNY42 <username.toNY42@gmail.com>
2026-03-27 12:10:16 +0300

    Update FASMHello.asm
    неправильно считаеться tmp2
    в формулe C(n,k) - tmp2 = k

--- FASMHello.asm
+++ FASMHello.asm
@@ -2,64 +2,98 @@ extern io_get_dec, io_print_dec
 
 section .bss
     n resd 1
-    k resd 1
-    m resd 1
     tmp resd 1
-    
+    tmp2 resd 1
+ 
+ 
 
 section .data
     res dd 0
+    cnt dd 0
 
 section .text
 global main
 main:
-    mov ebp, esp
+    mov ebp, esp; for correct debugging
+    ;write your code here
+    ;
     call io_get_dec
     mov dword[n], eax
     call io_get_dec
-    mov dword[k], eax
-    
-    ;Поиск позиции старшего бита (m) 
+    mov ebx, eax; ebx = k
     xor ecx, ecx
+    ;
+    ;ищем позицию старшего бита в числе n
+    mov eax, dword[n]
 .loop1:
-    shr dword[n], 1
-    cmp dword[n], 0
+    shr eax, 1
+    cmp eax, 0
     je .find_m
     inc ecx
     jmp .loop1
     
+    
 .find_m:
-    ;m = ecx (номер старшего бита)
-    mov eax, ecx
-    mov dword[m], eax
+    ; len = ecx
+    mov esi, ecx
    
-    ;Подготовка к вычислению числителя: i * (i-1) * ... * (i-k+1)
-    ;где i = m - 1
-    dec eax             ; eax = m - 1
-    mov edi, dword[m]
-    sub edi, dword[k]
-    inc edi             ; edi = m - k + 1 (нижняя граница множителя)
+.loop:
+    dec esi; позиция бита
+    cmp ecx, 0; ecx < 0 ?
+    jl .end_loop
+    cmp dword[cnt], ebx; cnt >= K ?
+    jge .end_loop
+    bt dword[n], esi
+    jc .is_one
+    inc dword[cnt]
+    dec ecx
+    jmp .loop
+    .is_one:    
+        mov eax, ecx
+        sub eax, ebx
+        add eax, dword[cnt]; eax = len - (k - cnt) + 1
+        inc eax
+        mov edi, eax
+        .c1:
+            inc edi
+            cmp edi, ecx; eax > len?
+            jg .end_c1
+            mul edi; eax *= edi
+            jmp .c1
+        
+        .end_c1:
+        ;eax = len * ... * (len -K + cnt)
+        mov dword[tmp], eax
+        mov eax, 1
+        mov edi, eax
+        .c2:
+            inc edi
+            cmp edi, ebx; 
+            jg .end_c2
+            mul edi; eax = *= edi
+            jmp .c2
+            
+        .end_c2:
+            mov dword[tmp2], eax
+            mov eax, dword[tmp]
+            xor edx, edx
+            div dword[tmp2]
+            ;eax = C(len, K - cnt)
+            add dword[res], eax
+            dec ecx
+            inc dword[cnt]
+            jmp .loop
     
-    mov dword[tmp], eax ; tmp = i (текущий множитель)
-    mov ebx, 1          ; ebx = 1 (аккумулятор результата)
+
+.end_loop:
+    cmp dword[cnt], ebx
+    jne .not_equal
+    inc dword[res]
     
-.loop2:
-    ; Сначала проверяем условие, потом умножаем
-    cmp dword[tmp], edi
-    jl .end_loop2       ; если tmp < edi, выходим
+    .not_equal:
     
-    mov eax, ebx        ; eax = текущий накопленный результат
-    mul dword[tmp]      ; eax = eax * tmp (результат в EDX:EAX)
-    mov ebx, eax        ; сохраняем младшую часть результата в ebx
-    
-    dec dword[tmp]      ; уменьшаем множитель
-    jmp .loop2
-    
-.end_loop2:
-    ; В ebx теперь находится числитель: i*(i-1)*...*(i-k+1) 
-.q2:
-    ; Здесь будет вычисление знаменателя k! и деление
-    
+    mov eax, dword[res]
+    call io_print_dec
     
     xor eax, eax
     ret
